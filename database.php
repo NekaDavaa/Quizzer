01fa1f902cd030ef0064ce8a3aba144734e709ca
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-08-29 17:35:45 +0300

    remove error handler because i cant fix stu xampp

--- database.php
+++ database.php
@@ -7,9 +7,3 @@ $db_pass = '121212aa';
 
 //Create mysqli object
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-//Error Handler
-if($mysqli->connect_error){
-	printf("Connect failed: %s\n", $mysqli->connect_error);
-	exit();
-}
