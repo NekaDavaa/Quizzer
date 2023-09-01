4ae7870f5dc168dc0a16a409f46f6fabf33a6fff
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-01 10:49:32 +0300

    Added ADD Question feature

--- add.php
+++ add.php
@@ -1,3 +1,45 @@
+<?php include 'database.php' ?>
+
+<?php
+
+$query = "select * from questions";
+$result = $mysqli->query($query);
+$next_question = mysqli_num_rows($result);
+
+if (isset($_POST['submit'])) {
+$question_number = $_POST['question_number'];
+$question_text = $_POST['question_text'];
+$choices = array();
+$choices[1] = $_POST['choice1'];
+$choices[2] = $_POST['choice2'];
+$choices[3] = $_POST['choice3'];
+$choices[4] = $_POST['choice4'];
+$choices[5] = $_POST['choice5'];
+$correct_choice = $_POST['correct_choice'];
+
+//The Query
+$query = "insert into questions (`question_number`, `text`) values ('$question_number', '$question_text')";
+//Run Query
+$insert_row = $mysqli->query($query) or die;
+
+if($insert_row) {
+  $is_correct = 0;
+  foreach ($choices as $choice => $value) {
+      if($choice == $correct_choice) {
+                 $is_correct = 1;
+      }
+       else {
+       	 $is_correct = 0;
+       } 
+ $query = "insert into `choices` (`question_number`, `is_correct`, `text`) values ('$question_number', '$is_correct', '$value')";
+ $choice_insert = $mysqli->query($query) or die;   
+
+ 
+}
+}
+}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -15,9 +57,9 @@
 		<div class="container">
 			<h2>Add A Question</h1>
 			<form method="post" action="add.php">
-				<p>
+				<p style="display: none;">
 					<label>Question Number: </label>
-					<input type="number" name="question_number" />
+					<input type="number" name="question_number" value="<?php echo $next_question + 1; ?>"/> 
 				</p>
 				<p>
 					<label>Question Text: </label>
